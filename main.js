84cecd49ef4c5d16226fbeb0f2053a020ce6d2c8
Артем <example@Yandex.ru>
2024-12-21 14:14:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+/*let a = prompt("Введите длину прямоугольника");
+let b = prompt("Введите ширину прямоугольника");
+
+a=Number(a); 
+b=Number(b); 
+
+let c = 2*(a+b);
+
+
+console.log("Периметр прямоугольника = ", c);
+
+alert (c)*/
+
+/*Обьем параллелограмма
+let a=prompt("Введите длину",6);
+let b=prompt("Введите ширину",6);
+let d=prompt("Введите высоту",6);
+console.log(typeof a);
+
+
+console.log(Number(a)*Number(b)*Number(d));
+*/
+
+
+/*
+console.log (14225435463%100);
+
+ 
+let a=prompt()
+
+if(a%2==0){
+ console.log("Четное");
+    
+}
+else{
+console.log("Нечетное");}
+*/
+
+/*
+let a=prompt();
+let b=prompt(a)
+
+if (a%2==b%2) {
+    console.log("ЧИсла имеют одинаковую четность");
+} else {
+    console.log("Числа не имеют одинаковую четность")
+}
+*/
+
+
+/*Домашнее*/
+
+let x=prompt();
+
+console.log(x*x+10*x+15);
+
